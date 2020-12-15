1cfdd4b1c5342c670bdadc650c313f74a646f84c
Eros Moreno <erosignacio.m@gmail.com>
2020-12-15 00:45:24 -0300

    Actualizacion Consultas
    Agregar consultas y su conexion a DB

--- src/Controllers/ControladorAcceso.java
+++ src/Controllers/ControladorAcceso.java
@@ -4,7 +4,10 @@ package Controllers;
 import Models.AccesoADO;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
 
 public class ControladorAcceso {
@@ -32,5 +35,110 @@ public class ControladorAcceso {
         }
         return con;
     }
+    public DefaultTableModel MostrarEvent(){
+        DefaultTableModel modelo = new DefaultTableModel();
+        
+        modelo.addColumn("ID");
+        modelo.addColumn("Complejo");
+        modelo.addColumn("Fecha");
+        modelo.addColumn("Hora inicio");
+        modelo.addColumn("Hora Termino");
+        try{
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from SOLICITUD");
+            while (rs.next()){//Verifica si hay datos
+            Object dato[] = new Object[5];
+                dato[0]=rs.getString("IDSOLICITUD");
+                dato[1]=rs.getString("IDCOMPLEJO");
+                dato[2]=rs.getString("FECHA");
+                dato[3]=rs.getString("HORAINICIO");
+                dato[4]=rs.getString("HORATERMINO");
+                modelo.addRow(dato);//Se llena una fila para las jTable
+            }
+            
+        }catch(Exception e){
     
+        }
+        return modelo;//Se envia el modelo
+    }
+    public DefaultTableModel MostrarComisario(){
+        DefaultTableModel modelo = new DefaultTableModel();
+        
+        modelo.addColumn("ID COMISARIO");
+        modelo.addColumn("Numero Evento");
+        modelo.addColumn("Rut");
+        modelo.addColumn("Nombre");
+        modelo.addColumn("Apellido Paterno");
+        modelo.addColumn("Apellido Materno");
+        modelo.addColumn("Telefono");
+        modelo.addColumn("Mail");
+        try{
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from COMISARIO");
+            while (rs.next()){//Verifica si hay datos
+            Object dato[] = new Object[8];
+                dato[0]=rs.getString("IDCOMISARIO");
+                dato[1]=rs.getString("IDEVENTO");
+                dato[2]=rs.getString("RUNCOM");
+                dato[3]=rs.getString("NOMBREC");
+                dato[4]=rs.getString("APPAT");
+                dato[5]=rs.getString("APMAT");
+                dato[6]=rs.getString("FONO");
+                dato[7]=rs.getString("EMAIL");
+                modelo.addRow(dato);//Se llena una fila para las jTable
+            }
+            
+        }catch(Exception e){
+    
+        }
+        return modelo;//Se envia el modelo
+    }
+    public DefaultTableModel MostrarDepo(){
+        DefaultTableModel modelo = new DefaultTableModel();
+        
+        modelo.addColumn("ID");
+        modelo.addColumn("Numero Evento");
+        modelo.addColumn("Nombre deportista");
+        modelo.addColumn("Apellido");
+        try{
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from DEPORTISTA");
+            while (rs.next()){//Verifica si hay datos
+            Object dato[] = new Object[4];
+                dato[0]=rs.getString("IDDEPOR");
+                dato[1]=rs.getString("IDEVENTO");
+                dato[2]=rs.getString("NOMBRE");
+                dato[3]=rs.getString("APELLIDO");
+                modelo.addRow(dato);//Se llena una fila para las jTable
+            }
+            
+        }catch(Exception e){
+    
+        }
+        return modelo;//Se envia el modelo
+    }
+    public DefaultTableModel MostrarMantencion(){
+        DefaultTableModel modelo = new DefaultTableModel();
+        
+        modelo.addColumn("ID");
+        modelo.addColumn("Numero Evento");
+        modelo.addColumn("Tipo mantencion");
+        modelo.addColumn("Descripcion");
+        try{
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from MANTENCION");
+            while (rs.next()){//Verifica si hay datos
+            Object dato[] = new Object[4];
+                dato[0]=rs.getString("IDMANTENCION");
+                dato[1]=rs.getString("IDEVENTO");
+                dato[2]=rs.getString("TIPO");
+                dato[3]=rs.getString("DESCRIPCION");
+                modelo.addRow(dato);//Se llena una fila para las jTable
+            }
+            
+        }catch(Exception e){
+    
+        }
+        return modelo;//Se envia el modelo
+    }
 }
